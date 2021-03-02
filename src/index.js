11aabe3c70a1d3b885926ab7e6d9b6c37d26bb53
Alex Hernandez <alexandre_2420@outlook.com>
2021-03-02 17:37:33 -0500

    instlacion webpack-babel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//componnets
+import App from './components/App'
+
+// -----------------------------------------------------------------------
+
+const container = document.getElementById('App')
+
+ReactDOM.render(<App />, container)
+
