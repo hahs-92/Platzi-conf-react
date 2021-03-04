618c0e2ce64e752fc5f3be417e8432cb7c2a4c93
Alex Hernandez <alexandre_2420@outlook.com>
2021-03-04 16:02:13 -0500

    reach helmet-allpages

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,6 +1,9 @@
 import { useRef, useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 
+//HELMET
+import { Helmet } from 'react-helmet'
+
 //CONTEXT
 import AppContext from '../context/AppContext'
 
@@ -44,67 +47,93 @@ const Information = () => {
 
 
     return(
-        <section className="Information">
+        <>
+            <Helmet>
+                <title>Platzi Conf -Info</title>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@TU_USER"/>
+                <meta name="twitter:creator" content="@TU_USER"/>
+                <meta name="twitter:title" content="Platzi Conf Store"/>
+                <meta name="twitter:description" content="Platzi Conf Store"/>
+                <meta
+                name="twitter:image"
+                content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:title" content="Platzi Conf Store"/>
+                <meta property="og:description" content="Platzi Conf Store"/>
+                <meta
+                property="og:image"
+                content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:url" content="platzi-conf-6a553.web.app" />
+                <meta property="og:site_name" content="Platzi Conf Store" />
+                <meta property="og:locale" content="es_ES" />
+                <meta property="og:type" content="article" />
+                <meta property="fb:app_id" content="ID_APP_FACEBOOK" />  
+            </Helmet>
 
-            <article className="Information-content">
-                <section className="Information-head">
-                    <h2>Informacion de Contacto</h2>
-                </section>
-                <section className="Information-form">
+            <section className="Information">
 
-                    <form ref= { form }>
-                        <label htmlFor="name"> Nombre Completo
-                            <input type="text" placeholder='Ingrese Nombre Completo' name='name'/>
-                        </label>
-                        <label htmlFor="email"> Correo Electronico
-                            <input type="email" placeholder='Ingrese Correo Electronico' name='email'/>
-                        </label>
-                        <label htmlFor="address">Direccion
-                            <input type="text" placeholder='Ingrese Direccion' name='address'/>
-                        </label>
-                        <label htmlFor="apto">Apto
-                            <input type="text" placeholder='Apto' name='apto'/>
-                        </label>
-                        <label htmlFor="country">Pais
-                            <input type="text" placeholder='Ingrese Pais' name='country'/>
-                        </label>
-                        <label htmlFor="state">Estado
-                            <input type="text" placeholder='Ingrese Estado' name='state'/>
-                        </label>
-                        <label htmlFor="city">Ciudad
-                            <input type="text" placeholder='Ingrese Ciudad' name='city'/>
-                        </label>
-                        <label htmlFor="cp">Codigo Postal
-                            <input type="text" placeholder='Ingrese Codigo Postal' name='cp'/>
-                        </label>
-                        <label htmlFor="phone">Telefono
-                            <input type="text" placeholder='Ingrese Telefono' name='phone'/>
-                        </label>
-                    </form>
-                </section>
-                <section className="Information-buttons">
-                    <div className="Information-back">
-                        <Link to='/checkout'>Regresar</Link>
-                    </div>
-                    <div className="Information-next">
-                        <button type='button' onClick={ handleSubmit }>Pagar</button>
-                    </div>
-                </section>
-            </article>
-            <aside className="Information-sidebar">
-                <h3>Pedido</h3>
+                <article className="Information-content">
+                    <section className="Information-head">
+                        <h2>Informacion de Contacto</h2>
+                    </section>
+                    <section className="Information-form">
 
-                { cart.map( item => (
-                    <article className="Information-item" key={ item.title }>
-                        <div className="Information-element">
-                            <h4>{ item.title }</h4>
-                            <span>{ item.price }</span>
+                        <form ref= { form }>
+                            <label htmlFor="name"> Nombre Completo
+                                <input type="text" placeholder='Ingrese Nombre Completo' name='name'/>
+                            </label>
+                            <label htmlFor="email"> Correo Electronico
+                                <input type="email" placeholder='Ingrese Correo Electronico' name='email'/>
+                            </label>
+                            <label htmlFor="address">Direccion
+                                <input type="text" placeholder='Ingrese Direccion' name='address'/>
+                            </label>
+                            <label htmlFor="apto">Apto
+                                <input type="text" placeholder='Apto' name='apto'/>
+                            </label>
+                            <label htmlFor="country">Pais
+                                <input type="text" placeholder='Ingrese Pais' name='country'/>
+                            </label>
+                            <label htmlFor="state">Estado
+                                <input type="text" placeholder='Ingrese Estado' name='state'/>
+                            </label>
+                            <label htmlFor="city">Ciudad
+                                <input type="text" placeholder='Ingrese Ciudad' name='city'/>
+                            </label>
+                            <label htmlFor="cp">Codigo Postal
+                                <input type="text" placeholder='Ingrese Codigo Postal' name='cp'/>
+                            </label>
+                            <label htmlFor="phone">Telefono
+                                <input type="text" placeholder='Ingrese Telefono' name='phone'/>
+                            </label>
+                        </form>
+                    </section>
+                    <section className="Information-buttons">
+                        <div className="Information-back">
+                            <Link to='/checkout'>Regresar</Link>
                         </div>
-                    </article>
-                ))}
+                        <div className="Information-next">
+                            <button type='button' onClick={ handleSubmit }>Pagar</button>
+                        </div>
+                    </section>
+                </article>
+                <aside className="Information-sidebar">
+                    <h3>Pedido</h3>
 
-            </aside>
-        </section>
+                    { cart.map( item => (
+                        <article className="Information-item" key={ item.title }>
+                            <div className="Information-element">
+                                <h4>{ item.title }</h4>
+                                <span>{ item.price }</span>
+                            </div>
+                        </article>
+                    ))}
+
+                </aside>
+            </section>
+        </>
     )
 
 }
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -3,6 +3,9 @@ import { useHistory } from 'react-router-dom'
 //PAYPALBUTTON
 import { PayPalButton } from 'react-paypal-button'
 
+//HELMET
+import { Helmet } from 'react-helmet'
+
 //CONTEXT
 import { useContext } from 'react'
 
@@ -59,38 +62,64 @@ const Payment = () => {
 
 
     return(
-        <section className="Payment">
-            <article className="Payment-content">
-                <h3>Resumen del Pedido:</h3>
+        <>
+            <Helmet>
+                <title>Platzi Conf -Payment</title>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@TU_USER"/>
+                <meta name="twitter:creator" content="@TU_USER"/>
+                <meta name="twitter:title" content="Platzi Conf Store"/>
+                <meta name="twitter:description" content="Platzi Conf Store"/>
+                <meta
+                name="twitter:image"
+                content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:title" content="Platzi Conf Store"/>
+                <meta property="og:description" content="Platzi Conf Store"/>
+                <meta
+                property="og:image"
+                content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:url" content="platzi-conf-6a553.web.app" />
+                <meta property="og:site_name" content="Platzi Conf Store" />
+                <meta property="og:locale" content="es_ES" />
+                <meta property="og:type" content="article" />
+                <meta property="fb:app_id" content="ID_APP_FACEBOOK" />  
+            </Helmet>
+            
+            <section className="Payment">
+                <article className="Payment-content">
+                    <h3>Resumen del Pedido:</h3>
 
-                { cart.map( item => (
-                    <article className="Payment-item" key={ item.title}>
-                        <div className="Payment-element">
-                            <h4>{ item.title }</h4>
-                            <span>{ `$ ${ item.price }` }</span>
-                        </div>
-                    </article>
-                ))}
+                    { cart.map( item => (
+                        <article className="Payment-item" key={ item.title}>
+                            <div className="Payment-element">
+                                <h4>{ item.title }</h4>
+                                <span>{ `$ ${ item.price }` }</span>
+                            </div>
+                        </article>
+                    ))}
 
-                <div className="Payment-button">
+                    <div className="Payment-button">
 
-                    <PayPalButton  
-                        paypalOptions ={ paypalOptions }
-                        buttonStyles = { buttonStyles }
-                        amount = { HandleSummTotal() }
-                        onPaymentStart = { () => console.log('start payment') } 
-                        onPaymentSuccess = { data => handlePaySuccess(data) }
-                        onPaymentError = { error => console.log(error) }
-                        onPaymentCancel = { data => console.log(data) }
-                    />
-                
-                </div>
-            </article>
+                        <PayPalButton  
+                            paypalOptions ={ paypalOptions }
+                            buttonStyles = { buttonStyles }
+                            amount = { HandleSummTotal() }
+                            onPaymentStart = { () => console.log('start payment') } 
+                            onPaymentSuccess = { data => handlePaySuccess(data) }
+                            onPaymentError = { error => console.log(error) }
+                            onPaymentCancel = { data => console.log(data) }
+                        />
+                    
+                    </div>
+                </article>
 
-            <aside>
-                
-            </aside>
-        </section>
+                <aside>
+                    
+                </aside>
+            </section>
+        </>
     )
 }
 
